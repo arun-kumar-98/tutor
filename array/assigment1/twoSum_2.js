1fc1a33df251d23908c457be474cd41f12ab5483
arun <arunrits15.com>
2023-06-01 15:11:08 +0530

    leetcode js challenge

--- array/assigment1/twoSum_2.js
+++ array/assigment1/twoSum_2.js
@@ -4,12 +4,51 @@
  * @return {number[]}
  */
 
+// var twoSum = function (numbers, target) {
+//   //take two pointer
+
+//   let start = 0;
+//   let end = numbers.length - 1;
+
+//   while (start < end) {
+//     if (numbers[start] + numbers[end] > target) {
+//       end--;
+//     } else if (numbers[start] + numbers[end] < target) {
+//       start++;
+//     } else {
+//       //return 1-based inexing array
+//       return [start + 1, end + 1];
+//     }
+//   }
+//   return [];
+// };
+// const numbers = [2, 7, 11, 15];
+// let target = 20;
+// console.log(twoSum(numbers, target));
+
+// const numbers1 = [2, 3, 4];
+// let target1 = 6;
+
+// console.log(twoSum(numbers1, target1));
+
+// const arr = [-1, 0];
+// const target2 = -1;
+// console.log(twoSum(arr, target2));
+
 var twoSum = function (numbers, target) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = numbers.length - 1; j > 0; j--) {
-      let sum = numbers[i] + numbers[j];
-      if (target < sum) {
-      }
+  let start = 0;
+  let end = numbers.length - 1;
+
+  while (start < end) {
+    if (numbers[start] + numbers[end] == target) {
+      return [start + 1, end + 1];
+    } else if (numbers[start] + numbers[end] > target) {
+      end--;
+    } else {
+      start++;
     }
   }
+  return [];
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ arrayWrapper.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function (nums) {
+  this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function () {
+  return this.nums.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+};
+
+ArrayWrapper.prototype.toString = function () {
+  // wrap with string
+
+  return JSON.stringify(this.nums);
+};
+
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+console.log(obj1 + obj2); // 10
+console.log(String(obj1)); // "[1,2]"
+console.log(String(obj2));
+// "[3,4]"
